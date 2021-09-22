09410c121ed388680d0d94b004d12c5ea148fcf9
Ninnjah <ninnjah.mail@gmail.com>
2021-09-22 19:04:30 +0300

    Add yes_or_not function

--- utils/cli.py
+++ utils/cli.py
@@ -4,3 +4,15 @@ def get_number():
             return int(input("Enter password length: "))
         except ValueError:
             print("You should enter a number")
+
+
+def yes_or_not(msg):
+    print(msg)
+    while True:
+        answer = input("Enter y/n: ")
+        if answer.lower() == "y":
+            return True
+        elif answer.lower() == "n":
+            return False
+        else:
+            print("You should enter y/n")
