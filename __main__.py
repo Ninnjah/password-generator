c12d9e3d7987fb8d654a0309126c65472463a26f
Ninnjah <ninnjah.mail@gmail.com>
2021-09-22 18:30:57 +0300

    Added password generation function instead of PasswordGenerator class

--- __main__.py
+++ __main__.py
@@ -1,6 +1,18 @@
-from utils.passgen import PasswordGenerator
+import string
+import secrets
+
+
+def generate_password(length: int = 8, alphabet: str = string.ascii_letters + string.digits + "!?_-") -> str:
+    return ''.join(secrets.choice(alphabet) for i in range(length))
 
 
 if __name__ == "__main__":
-    passgenerator = PasswordGenerator()
-    print(passgenerator.generate_password())
+    while True:
+        try:
+            pass_length = int(input("Enter password length: "))
+            break
+        except ValueError:
+            print("You should enter a number")
+
+    print(generate_password(pass_length))
+    input()
