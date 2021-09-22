28671721993a207b514dca2fa9a8e92aab00fca6
Ninnjah <ninnjah.mail@gmail.com>
2021-09-22 19:05:55 +0300

    Added type hints

--- utils/cli.py
+++ utils/cli.py
@@ -1,4 +1,4 @@
-def get_number():
+def get_number() -> int:
     while True:
         try:
             return int(input("Enter password length: "))
@@ -6,10 +6,10 @@ def get_number():
             print("You should enter a number")
 
 
-def yes_or_not(msg):
+def yes_or_not(msg: str) -> bool:
     print(msg)
     while True:
-        answer = input("Enter y/n: ")
+        answer: str = input("Enter y/n: ")
         if answer.lower() == "y":
             return True
         elif answer.lower() == "n":
