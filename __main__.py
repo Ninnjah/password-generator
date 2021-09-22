da79f007d9d36480ebdbe75e1a8fb270d2350a88
Ninnjah <ninnjah.mail@gmail.com>
2021-09-22 19:10:12 +0300

    Add cli module and used get_number function for input password length

--- __main__.py
+++ __main__.py
@@ -1,18 +1,14 @@
 import string
 import secrets
 
+from utils import cli
+
 
 def generate_password(length: int = 8, alphabet: str = string.ascii_letters + string.digits + "!?_-") -> str:
     return ''.join(secrets.choice(alphabet) for i in range(length))
 
 
 if __name__ == "__main__":
-    while True:
-        try:
-            pass_length = int(input("Enter password length: "))
-            break
-        except ValueError:
-            print("You should enter a number")
-
+    pass_length = cli.get_number()
     print(generate_password(pass_length))
     input()
