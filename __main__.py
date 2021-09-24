a193b9973a1085f5fa7b48a3d670ea2ffc41a54e
Ninnjah <ninnjah.mail@gmail.com>
2021-09-24 20:32:35 +0300

    Add config_words_pass function

--- __main__.py
+++ __main__.py
@@ -1,11 +1,25 @@
+import os
 import string
 import secrets
+from typing import List
 
 from utils import cli
 
 
 def generate_password(length: int, alphabet: tuple, separator: str = "") -> str:
     return separator.join(secrets.choice(alphabet) for _ in range(length))
+
+
+def config_words_pass() -> None:
+    file_path: str = "./dictionaries"
+    file_list: List[str] = [x for x in os.listdir(file_path) if os.path.isfile(os.path.join(file_path, x))]
+
+    file_name = os.path.join(file_path, cli.choose("Choose dict file: ", file_list))
+    with open(file_name, "r", encoding="utf-8") as f:
+        word_list: tuple = tuple(_.strip() for _ in f.readlines())
+
+    pass_length = cli.get_number("Enter words count in password: ")
+    print(generate_password(pass_length, word_list, separator="-"))
 
 
 if __name__ == "__main__":
